254c392b5b02833f39d6199384458b14dea18c4d
Julian Hammer <jh@moinjulian.com>
2024-02-23 22:42:00 +0000

    Add mongodb

---
+++ connect_to_db.py
@@ -0,0 +1,14 @@
+import datetime   # This will be needed later
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load config from a .env file:
+load_dotenv()
+MONGODB_URI = os.environ['MONGODB_URI']
+
+# Connect to your MongoDB cluster:
+client = MongoClient(MONGODB_URI)
+
+print(client.list_databases)
