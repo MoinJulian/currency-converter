797724740f6c7665ffac7ac34cae9af738346809
Julian Hammer <jh@moinjulian.com>
2024-02-29 13:49:38 +0000

    Comment code

--- main.py
+++ main.py
@@ -16,16 +16,19 @@ print(wakatime_response.status_code)
 img_data = wakatime_response.content
 
 def open_github():
+    # Open GitHub profile in web browser.
     webbrowser.open("https://github.com/MoinJulian")
 
 
 def create_main_window():
+    # Create the main window for the currency converter application.
     root = tk.Tk()
     root.title("Currency Converter")
     return root
 
 
 def add_info_label(root):
+    # Add an information label to the main window.
     info_label = ttk.Label(root, text="Please note that this program is not using an API and therefore can't have "
                                        "up-to-date information.")
     info_label.grid(row=0, column=0, columnspan=2, pady=10)
@@ -33,6 +36,7 @@ def add_info_label(root):
 
 
 def add_choice_section(root):
+    # Add the currency conversion choice section to the main window.
     choice_label = ttk.Label(root, text="Select conversion:")
     choice_label.grid(row=1, column=0, pady=5, sticky="e")
 
@@ -44,6 +48,7 @@ def add_choice_section(root):
 
 
 def add_amount_section(root):
+    # Add the amount entry section to the main window.
     amount_label = ttk.Label(root, text="Enter amount:")
     amount_label.grid(row=2, column=0, pady=5, sticky="e")
 
@@ -53,6 +58,7 @@ def add_amount_section(root):
 
 
 def add_convert_button(root, choice_var, choice_combobox, amount_entry, result_label, result_link):
+    # Add the convert button to the main window.
     def convert():
         perform_currency_conversion(choice_var, choice_combobox, amount_entry, result_label, result_link, CONVERSION_RATES)
 
@@ -61,6 +67,7 @@ def add_convert_button(root, choice_var, choice_combobox, amount_entry, result_l
 
 
 def add_result_labels(root):
+    # Add the result labels to the main window.
     result_label = ttk.Label(root, text="")
     result_label.grid(row=4, column=0, columnspan=2, pady=10)
 
@@ -71,13 +78,18 @@ def add_result_labels(root):
 
 
 def add_github_button(root):
+    # Add the GitHub button to the main window.
     button = ttk.Button(root, text="GitHub", command=open_github)
     button.grid(row=6, column=0, columnspan=2, pady=20)
 
+
 def open_waka_time():
+    # Open WakaTime profile in web browser.
     webbrowser.open("https://wakatime.com/@MoinJulian")
 
+
 def load_wakatime_badge(root, img_data):
+    # Load and display the WakaTime badge in the main window.
     # Convert SVG data to PNG
     png_data = cairosvg.svg2png(bytestring=img_data)
 
@@ -88,7 +100,9 @@ def load_wakatime_badge(root, img_data):
     panel.image = img
     panel.grid(row=7, column=0, columnspan=2, pady=10)
 
+
 def main():
+    # Main function to create and run the currency converter application.
     root = create_main_window()
     add_info_label(root)
     load_wakatime_badge(root, img_data)
