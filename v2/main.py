bfdab923874eea547147ccbdf3426184094771a7
Julian Hammer <jh@moinjulian.com>
2024-03-21 15:18:33 +0000

    Excluded all out of date currencies

--- v2/main.py
+++ v2/main.py
@@ -5,9 +5,6 @@ from currency_converter import CurrencyConverter
 root = Tk()
 root.title("Currency Converter")
 
-# Currencies to convert
-currencies = ["EUR", "USD", "GBP", "CAD", "JPY", "CNY", "RUB", "AUD", "NZD", "BRL", "INR", "ZAR", "SEK", "CHF", "HKD"]
-
 # Create StringVar variables to store the selected currencies
 currency_val1 = StringVar(root)
 currency_val2 = StringVar(root)
@@ -17,9 +14,19 @@ currency_val2.set("To")
 # Create an instance of the CurrencyConverter class
 c = CurrencyConverter()
 
+# Currencies to convert
+currencies = list(c.currencies)
+
+currencies = list(c.currencies)
+excluded_currencies = ["ROL", "CYP", "TRL", "LVL", "MTL"]
+
+for currency in excluded_currencies:
+    if currency in currencies:
+        currencies.remove(currency)
+
 # Create StringVar variables to store the input and output currency values
-currecy_input = StringVar(root)
-currecy_output = StringVar(root)
+currency_input = StringVar(root)
+currency_output = StringVar(root)
 
 # Set the title and size of the root window
 root.title("Currency Converter")
@@ -41,7 +48,7 @@ def convert_cur(amount, fromm, too):
         # TODO: Add implementation
         pass
     else:
-        currecy_output.set(round(c.convert(amount, fromm.get(), too.get()), 5))
+        currency_output.set(round(c.convert(amount, fromm.get(), too.get()), 5))
 
 # Function to validate the input in the currency input field
 def entry_valid(input):
@@ -54,9 +61,9 @@ def entry_valid(input):
     Returns:
     bool: True if the input is valid, False otherwise.
     """
-    global currency_val1, currency_val2, currecyinput
+    global currency_val1, currency_val2, currency_input
     if input[-1] == "." or input[-1].isdigit():
-        currecyinput = input
+        currency_input = input
         if currency_val1.get() != "From" and currency_val2.get() != "To":
             convert_cur(input, currency_val1, currency_val2)
             return True
@@ -72,15 +79,15 @@ def callback(*args):
     """
     Callback function to update the currency conversion when the selected currencies change.
     """
-    global currecyinput, currency_val1, currency_val2
-    convert_cur(currecyinput, currency_val1, currency_val2)
+    global currency_input, currency_val1, currency_val2
+    convert_cur(currency_input, currency_val1, currency_val2)
 
 # Create a label widget for the title
 label = Label(root, text='Currency Converter', font="Helvetica 40 bold")
 label.pack()
 
 # Create an entry widget for the currency input
-amount = Entry(root, textvariable=currecy_input, font="Geneva 30 bold", justify=CENTER)
+amount = Entry(root, textvariable=currency_input, font="Geneva 30 bold", justify=CENTER)
 amount.pack(padx=10)
 
 # Register the entry_valid function as a validation command for the amount entry widget
@@ -102,7 +109,7 @@ currency_val1.trace('w', callback)
 currency_val2.trace('w', callback)
 
 # Create an entry widget for the currency output
-output = Entry(root, textvariable=currecy_output, font="Geneva 30 bold", justify=CENTER)
+output = Entry(root, textvariable=currency_output, font="Geneva 30 bold", justify=CENTER)
 output.pack()
 
 # Start the main event loop
